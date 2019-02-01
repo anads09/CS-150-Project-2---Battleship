3648813f3454ca9af34219ec9d450ff1a05cc885
Austin Nadler <austinnadler706@gmail.com>
2019-01-31 23:06:59 -0600

    Update Space.java

--- Space.java
+++ Space.java
@@ -1,10 +1,4 @@
-/*
-Austin Nadler
-CS150-002
-Program #2
-10/05/2018
-*/
-package cs150proj02;
+package battelship;
 
 public class Space {  
     
@@ -16,43 +10,22 @@ public class Space {
     private boolean hasShip;
     private boolean hasBeenShotAt = false;
     
+    public String getValue() { return value; }    
+    public void setValueToBLANK() { value = BLANK;}    
+    public void setValueToMISS() { value = MISS; }    
+    public void setValueToNEAR() { value = NEAR; }
+    public void setValueToHIT() { value = HIT; }    
+    public void setShip() { hasShip = true; }    
+    public void destroyShip() { hasShip = false; }    
+    public boolean doesSpaceHaveShip() { return hasShip; }
+    
     public Space () {
         value = BLANK;
         hasShip = false;
         hasBeenShotAt = false;
     }
     
-    public String getValue() {
-        return value;
-    }
     
-    public void setValueToBLANK() {
-        value = BLANK;
-    }
-    
-    public void setValueToMISS() {
-        value = MISS;
-    }
-    
-    public void setValueToNEAR() {
-        value = NEAR;
-    }
-    
-    public void setValueToHIT() {
-        value = HIT;
-    }
-    
-    public void setShip() {
-        hasShip = true;
-    }
-    
-    public void destroyShip() {
-        hasShip = false;
-    }
-    
-    public boolean doesSpaceHaveShip() {
-        return hasShip;
-    }
     
     public boolean hasSpaceBeenShotAt() {
         if (value != BLANK) {
